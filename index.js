a6cc48d53f968b396fdd926d984d23e2f2bd91e8
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-10-10 02:29:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution here
 function findMatching(array, string) {
-  return array.filter(driver => driver.toUpperCase() === name.toUppercase())
+  return array.filter(driver => driver.toUpperCase() === string.toUppercase())
 }
